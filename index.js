c5d368181a4e901a77702a39c512efc2ea2ba18d
BhanuPrakash-Guniganti <gunigantibhanu@gmail.com>
2025-10-14 19:56:56 +0530

    Practised Schema And MOdels

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/myDataBase')
+.then(()=> console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
+
+
+//Schema
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    creator: String,
+    publishedDate: { type: Date, default: Date.now },
+    isPublished: Boolean
+})
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse(){
+    const course = new Course({
+        name:'Express',
+        creator:'Prakash',
+        isPublished:false
+    })
+
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
