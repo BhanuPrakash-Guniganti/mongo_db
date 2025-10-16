3105e4a2bd6e539ac5840b1872897e24e312a23b
BhanuPrakash-Guniganti <gunigantibhanu@gmail.com>
2025-10-16 16:01:08 +0530

    Logical Query Operators

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ async function createCourse(){
 //createCourse();
 
 
+//Comparison Query Operators
+
 //eq(equal)
 //ne(not equal)
 //gt(greater than)
@@ -46,49 +48,22 @@ async function createCourse(){
 
 
 
+//Logical Query Operators
+//or
+//and
 
 async function getCourses(){
-    const courses = await Course.find({rating : {$gte : 4}}).select({name:1,publishedDate:1})
-    console.log(courses);
+
+const courses = await Course.find({
+    $and: [
+        { rating: { $in: [4.1, 3.5, 4.6] } },
+        { creator: 'Prakash' }
+    ]
+})
+.select({ name: 1, publishedDate: 1 });
+
+console.log(courses);
 
 }
 
 getCourses();
-
-/*
-
-const mongoose = require('mongoose');
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/myDataBase')
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Could not connect to MongoDB', err));
-
-// Schema
-const courseSchema = new mongoose.Schema({
-    name: String,
-    creator: String,
-    publishedDate: { type: Date, default: Date.now },
-    isPublished: Boolean
-});
-
-const Course = mongoose.model('Course', courseSchema);
-
-// Function to create and fetch course
-async function createAndGetCourse() {
-    // Create a new course
-    const course = new Course({
-        name: 'Express',
-        creator: 'Mrinal',  // match your query
-        isPublished: true
-    });
-    await course.save();
-
-    // Fetch courses created by 'Mrinal'
-    const courses = await Course.find({ creator: 'Mrinal' });
-    console.log(courses);
-}
-
-// Call the function
-createAndGetCourse();
-*/
